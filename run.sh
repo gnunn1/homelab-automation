75afb2bfbe913c25c87b34d5c32bafcd20164ac1
Gerald Nunn <gerald.b.nunn@gmail.com>
2026-01-17 09:02:44 -0800

    Update to handle certs for UDR7

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
- ansible-playbook -i inventory configure-infra-server.yaml --vault-password-file=.vault_pass
+ ansible-playbook -i inventory configure-infra-server.yaml --vault-password-file=.vault_pass --ask-become-pass
